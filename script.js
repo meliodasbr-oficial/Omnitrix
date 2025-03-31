3ebfc6c91d75d278e9148ea39a0a14ace0c5f1a1
Meliodas Br <meliodas.br.oficial@gmail.com>
2025-03-31 19:10:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const characters = [
+    "chama", "diamante", "4bracos", "xlr8", "ultrat", "fantasmatico", "besta", "insectoide", "gigante",
+    "enormossauro", "friagem", "ecoeco", "fogofatuo", "cromatico", "estrelapolar", "feedback", "shocksquatch",
+    "macacoaranha", "vilgax", "rook", "kevin", "gwen", "rex", "rath", "rng", "gravattack", "blitzwolfer", "atomico",
+    "ciposelvagem", "baladecanhao", "megaolhos", "glutao", "idem", "arraiaajato", "diabrete", "iguana",
+    "ameacaaquatica", "alienx"
+];
+
+let currentIndex = 0;
+
+function updateCharacters() {
+    const prevIndex = (currentIndex - 1 + characters.length) % characters.length;
+    const nextIndex = (currentIndex + 1) % characters.length;
+    
+    document.getElementById("prev-character").innerHTML = `<img src="images/${characters[prevIndex]}.png" alt="${characters[prevIndex]}">`;
+    document.getElementById("current-character").innerHTML = `<img src="images/${characters[currentIndex]}.png" alt="${characters[currentIndex]}">`;
+    document.getElementById("next-character").innerHTML = `<img src="images/${characters[nextIndex]}.png" alt="${characters[nextIndex]}">`;
+}
+
+document.getElementById("prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + characters.length) % characters.length;
+    updateCharacters();
+});
+
+document.getElementById("next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % characters.length;
+    updateCharacters();
+});
+
+updateCharacters();
